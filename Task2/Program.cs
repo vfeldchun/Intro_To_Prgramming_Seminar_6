20fd84d6c92c51bfaac3c74e528384221c1ad18b
vfeldchun <vladuz@mail.ru>
2024-02-23 21:00:03 +0300

    Solution for Task2 added!

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,3 +1,22 @@
 ﻿// Задача 2: 
 // Задайте строку, содержащую латинские буквы в обоих регистрах. 
 // Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+string inputString = "“aBcD1ef!-";
+
+string resultString = GetLowerCaseString(inputString);
+System.Console.WriteLine(resultString);
+// Простой вариант
+// System.Console.WriteLine(inputString.ToLower());
+
+
+string GetLowerCaseString(string inputString)
+{
+    string resultString = "";
+
+    foreach (var ch in inputString)
+        if (char.IsUpper(ch)) resultString += char.ToLower(ch);
+        else resultString += ch;
+
+    return resultString;
+}
