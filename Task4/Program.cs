94acea70fd2fad5ca40749ba6b7ce8ddb69a21ea
vfeldchun <vladuz@mail.ru>
2024-02-23 21:57:38 +0300

    Solution for Task4 added!

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -2,3 +2,28 @@
 // Задайте строку, состоящую из слов, разделенных пробелами. 
 // Сформировать строку, в которой слова расположены в обратном порядке. 
 // В полученной строке слова должны быть также разделены пробелами.
+
+string inputString = "Мама мыла раму";
+
+string[] wordArray = inputString.Trim().Split(' ');
+ReversedWordArray(wordArray);
+
+// Можно короче
+// wordArray.Reverse();
+
+System.Console.WriteLine(string.Join(" ", wordArray));
+
+
+void ReversedWordArray(string[] wordArray)
+{
+    string tempWord = "";
+    for (int i = 0; i <= wordArray.Length / 2; i++)
+    {
+        tempWord = wordArray[^(i + 1)];
+        wordArray[^(i + 1)] = wordArray[i];
+        wordArray[i] = tempWord;
+    }
+}
+
+
+
